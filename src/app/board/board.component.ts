21f1d3ec2f77c13bb85fdd77d6880e19a8b6c6d7
Kris <87007257+kris04090513@users.noreply.github.com>
2022-06-27 01:11:17 +0800

    list-board json

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -11,14 +11,15 @@ export class BoardComponent implements OnInit {
   title2: string = "外幣帳戶";
   title3: string = "數位帳戶";
 
-  eyeOpen = { open: true, close: false };
+  eyeOpen = { open: true, close: false }; // 眼睛icon開關
   amount: string = "22450";
   // boardSub: string[] = ["臺幣", "外幣", "數位"]
-  taiwanAccounts: string[] = ['臺幣帳戶', '1234-0000-0000-1253', '1234-5555-6666-7777']
+  taiwanAccounts: string[] = ['臺幣帳戶', '1234-0000-0000-1253', '1234-5555-6666-7777']// 下拉選單選擇對應帳戶
   boardSub: string = "台幣"
   @ViewChild('select') select!: ElementRef;
+  twCount: string[] = ["120,111", "123,002", "555,222"];// 下拉選單對應帳戶的＄＄對應
 
-  twCount: string[] = ["120,111", "123,002", "555,222"];
+  cardShow = { card01: true, card02: false }; // 顯示不同卡片開關
   constructor() { }
 
   ngOnInit(): void {
@@ -41,16 +42,35 @@ export class BoardComponent implements OnInit {
     console.log(v.currentTarget.dataset.value);
     this.boardSub = v.currentTarget.dataset.value;
     this.amount = v.currentTarget.dataset.money;
-    
 
 
-    
-    if (this.boardSub === "臺幣") {
-      const countSelect = this.select.nativeElement.value;
-      this.amount = this.twCount[countSelect];
-      if (countSelect == 1) {
-  
-       }
+    // if (this.boardSub === "臺幣") {
+    //   const countSelect = this.select.nativeElement.value;
+    //   this.amount = this.twCount[countSelect];
+    //   if (countSelect == 1) {
+
+    //   }
+    // }
+  }
+
+  accountChange(v: any): void {
+    const accountSelect = this.select.nativeElement.value; // 去找到點到的是第幾個select 的選項
+
+    console.log("countSelect", accountSelect);
+    this.amount = this.twCount[accountSelect];
+    // 換卡面
+    if (accountSelect === 0) {
+      this.cardShow.card01 = true;
+      this.cardShow.card02 = false;
     }
+
+    if (accountSelect === 1) {
+      this.cardShow.card01 = true;
+      this.cardShow.card02 = false;
+    } else if (accountSelect === 2) {
+      this.cardShow.card01 = false;
+      this.cardShow.card02 = true;
+    }
+
   }
 }
--- src/app/list-board/list-board.component.ts
+++ src/app/list-board/list-board.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+// import { Http, Response, Headers, RequestOptions } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
 
+// https://ithelp.ithome.com.tw/articles/10186703
+import { HttpErrorResponse, HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { catchError, retry, timeout } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+@Injectable()
 @Component({
   selector: 'app-list-board',
   templateUrl: './list-board.component.html',
   styleUrls: ['./list-board.component.css']
 })
 export class ListBoardComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
+    // const result = new Subject<any>(); 
+    // result.subscribe((rs) => {
+    //   console.log(rs);
+    // });
+
+    // result.next("1");
+    // result.next("2");
+
   }
 
+  // private _jsonURL = 'assets/data/list-data.json';
+
+ 
 }
